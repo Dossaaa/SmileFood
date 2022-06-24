1039167ddfb99c645759f69a04698aef58fc859f
Dossaaa <jumpstyle.tds@gmail.com>
2022-06-24 19:17:29 +0300

    all

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function name() {
   var mixer = mixitup('.popular-menu__inner');
+
 });
+
+
+$ (function name(){
+  
+  $('.comments-content__slider').slick({
+    dots :true,
+    arrows: true,
+  });
+}) 
